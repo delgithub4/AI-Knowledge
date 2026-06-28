8230385950d2d1d1c9e595d1ce34c2cfb4cae2e7
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-28 15:44:18 +0100

    Create knowledge_repository.py

---
+++ repositories/knowledge_repository.py
@@ -0,0 +1,23 @@
+class KnowledgeRepository:
+
+    def collections(self):
+
+        return [
+
+            {
+
+                "id":1,
+
+                "name":"Policies"
+
+            },
+
+            {
+
+                "id":2,
+
+                "name":"Documentation"
+
+            }
+
+        ]
